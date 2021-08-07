076fbf2dda05a5f6d711b978c7f2e31fc1acdeb6
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-08-07 17:47:08 +0530

    Initial-commit

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+// ACCORDION FUNCTION //
+const accordionBoxes = document.querySelectorAll(".accordion__card");
+accordionBoxes.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.classList.toggle("notactive");
+  });
+});
+
+// NAVBAR FUNCTION //
+
+// STICKY NAVBAR //
+
+// SCROLL TOP FUNCTION //
