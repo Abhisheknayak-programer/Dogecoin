3fab29a081aad307ef6db0e5a6083ac1483b6cdc
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-08-08 09:33:49 +0530

    Initial commit

--- public/js/script.js
+++ public/js/script.js
@@ -7,12 +7,23 @@ accordionBoxes.forEach((element) => {
 });
 
 // NAVBAR FUNCTION //
+const navLink = document.querySelector(".nav_link");
+const navLinksHolder = document.querySelector(".nav_links");
+const burger = document.querySelector(".burger");
+const line1 = document.querySelector(".line__1");
+const line2 = document.querySelector(".line__2");
+const line3 = document.querySelector(".line__3");
+
+burger.addEventListener("click", () => {
+  navLinksHolder.classList.toggle("navActive");
+  burger.classList.toggle("toggle");
+});
 
 // STICKY NAVBAR //
 
 // TEXT ANIMATION //
 const NumHolder = document.querySelector(".Num_holder");
-// let numberCounting = setInterval(numsDone, 100);
+let numberCounting = setInterval(numsDone, 70);
 let count = 14303;
 function numsDone() {
   count = count + 1;
