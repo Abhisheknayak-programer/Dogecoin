3baf1fd4e315ba93be51ff15528a970d6ffbdeb2
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-08-08 08:03:41 +0530

    some changes done [wave additional design and mobile responsive]

--- public/js/script.js
+++ public/js/script.js
@@ -10,4 +10,21 @@ accordionBoxes.forEach((element) => {
 
 // STICKY NAVBAR //
 
+// TEXT ANIMATION //
+const NumHolder = document.querySelector(".Num_holder");
+// let numberCounting = setInterval(numsDone, 100);
+let count = 14303;
+function numsDone() {
+  count = count + 1;
+  NumHolder.textContent = count;
+  if (count > 30000) {
+    count = 1500;
+  }
+}
+
 // SCROLL TOP FUNCTION //
+const upperPart = document.querySelector("main");
+const scrollUpBox = document.querySelector(".scrollUpBox");
+scrollUpBox.addEventListener("click", () => {
+  upperPart.scrollIntoView({ behavior: "smooth" });
+});
